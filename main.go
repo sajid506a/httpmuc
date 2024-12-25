a469d5b09c99f7f40c6858c5c2cefd2674b90c61
sajid506a <sajid506a@gmail.com>
2024-12-25 21:43:02 +0530

    new files added

--- main.go
+++ main.go
@@ -1,19 +1,63 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+	"sync"
 )
+
+type User struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Age         string `json:"age"`
+}
+
+var userCache = make(map[int]User)
+var cacheMutex sync.RWMutex
+var users []User
 
 func main() {
 
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", Home)
+	mux.HandleFunc("POST /users", createUser)
 	fmt.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", mux)
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello home")
+	usersString := strings.Join(usersToStringSlice(users), "\n")
+
+	fmt.Fprintf(w, usersString)
+}
+
+func usersToStringSlice(users []User) []string {
+	result := make([]string, len(users))
+	for i, u := range users {
+		result[i] = fmt.Sprintf("%+v", u)
+	}
+	return result
+}
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	fmt.Println(err)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if user.Name == "" {
+		http.Error(w, "username is required", http.StatusBadRequest)
+		return
+	}
+
+	cacheMutex.Lock()
+	userCache[len(userCache)+1] = user
+	cacheMutex.Unlock()
+	users = append(users, user)
+	w.WriteHeader(http.StatusNoContent)
 }
